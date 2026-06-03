bfb7c06652d7760581b23045dfb531807310326c
hnuen <mn.nuen@gmail.com>
2026-06-02 21:00:25 -0400

    fix: add default export structure for cloudflare

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,24 +1,21 @@
-export default {
+import type { OpenNextConfig } from "@opennextjs/cloudflare";
+
+const config: OpenNextConfig = {
   default: {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
       proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "direct",
     },
   },
-  edgeExternals: ["node:crypto"],
   middleware: {
     external: true,
     override: {
       wrapper: "cloudflare-edge",
       converter: "edge",
       proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "direct",
     },
   },
 };
+
+export default config;
