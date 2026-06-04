2252118610aa1aee876c4ef0c1c13cc613f5d056
hnuen <mn.nuen@gmail.com>
2026-06-04 17:20:26 -0400

    put _worker.js in assets with correct relative paths

--- scripts/patch-worker.js
+++ scripts/patch-worker.js
@@ -1,8 +1,19 @@
 const fs = require('fs');
-const worker = fs.readFileSync('.open-next/worker.js', 'utf8');
-const patched = worker
-  .replace(/export \{ DOQueueHandler \} from "\.\/\.build\/durable-objects\/queue\.js";\n/, '')
-  .replace(/export \{ DOShardedTagCache \} from "\.\/\.build\/durable-objects\/sharded-tag-cache\.js";\n/, '')
-  .replace(/export \{ BucketCachePurge \} from "\.\/\.build\/durable-objects\/bucket-cache-purge\.js";\n/, '');
-fs.writeFileSync('.open-next/_worker.js', patched);
-console.log('Worker patched. durable-objects refs:', (patched.match(/durable-objects/g)||[]).length);
+let worker = fs.readFileSync('.open-next/worker.js', 'utf8');
+
+// Remove durable-objects exports
+worker = worker
+  .replace(/export \{ DOQueueHandler \} from "\.\/\.build\/durable-objects\/queue\.js";\n?/g, '')
+  .replace(/export \{ DOShardedTagCache \} from "\.\/\.build\/durable-objects\/sharded-tag-cache\.js";\n?/g, '')
+  .replace(/export \{ BucketCachePurge \} from "\.\/\.build\/durable-objects\/bucket-cache-purge\.js";\n?/g, '');
+
+// Fix import paths to point to parent directory (since worker will be in assets/)
+worker = worker
+  .replace(/from "\.\/cloudflare\//g, 'from "../cloudflare/')
+  .replace(/from "\.\/middleware\//g, 'from "../middleware/')
+  .replace(/import\("\.\/server-functions\//g, 'import("../server-functions/');
+
+// Write _worker.js into assets folder
+fs.writeFileSync('.open-next/assets/_worker.js', worker);
+console.log('Worker written to .open-next/assets/_worker.js');
+console.log('durable-objects refs:', (worker.match(/durable-objects/g)||[]).length);
