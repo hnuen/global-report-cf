65ea5b5e83d897cdb781349c47ddf80b42bc176e
hnuen <mn.nuen@gmail.com>
2026-06-04 18:11:25 -0400

    fix static files copy assets to .open-next root

--- scripts/patch-worker.js
+++ scripts/patch-worker.js
@@ -1,19 +1,13 @@
 const fs = require('fs');
 let worker = fs.readFileSync('.open-next/worker.js', 'utf8');
 
-// Remove durable-objects exports
+// Remove missing durable-objects exports
 worker = worker
   .replace(/export \{ DOQueueHandler \} from "\.\/\.build\/durable-objects\/queue\.js";\n?/g, '')
   .replace(/export \{ DOShardedTagCache \} from "\.\/\.build\/durable-objects\/sharded-tag-cache\.js";\n?/g, '')
   .replace(/export \{ BucketCachePurge \} from "\.\/\.build\/durable-objects\/bucket-cache-purge\.js";\n?/g, '');
 
-// Fix import paths to point to parent directory (since worker will be in assets/)
-worker = worker
-  .replace(/from "\.\/cloudflare\//g, 'from "../cloudflare/')
-  .replace(/from "\.\/middleware\//g, 'from "../middleware/')
-  .replace(/import\("\.\/server-functions\//g, 'import("../server-functions/');
-
-// Write _worker.js into assets folder
-fs.writeFileSync('.open-next/assets/_worker.js', worker);
-console.log('Worker written to .open-next/assets/_worker.js');
-console.log('durable-objects refs:', (worker.match(/durable-objects/g)||[]).length);
+// Write as _worker.js in .open-next root (same directory as cloudflare/, middleware/, server-functions/)
+fs.writeFileSync('.open-next/_worker.js', worker);
+console.log('_worker.js written to .open-next/');
+console.log('durable-objects refs remaining:', (worker.match(/durable-objects/g)||[]).length);
